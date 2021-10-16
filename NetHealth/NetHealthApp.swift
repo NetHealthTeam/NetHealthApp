645c5f4d67d191268ade9208aefab0607f363995
Anday <48630069+anday2000@users.noreply.github.com>
2021-10-16 23:32:04 +0200

    Initial Commit

---
+++ NetHealth/NetHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetHealthApp.swift
+//  NetHealth
+//
+//  Created by Anday on 16.10.21.
+//
+
+import SwiftUI
+
+@main
+struct NetHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
